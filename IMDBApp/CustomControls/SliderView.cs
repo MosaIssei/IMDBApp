405cb794b8e9d05eac9e831fa003949501933435
ISSEI <mohammadsb85@gmail.com>
2022-04-10 10:22:23 +0430

    Add project files.

---
+++ IMDBApp/CustomControls/SliderView.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IMDBApp.CustomControls
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:IMDBApp.CustomControls"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:IMDBApp.CustomControls;assembly=IMDBApp.CustomControls"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:SliderView/>
+    ///
+    /// </summary>
+    public class SliderView : Control
+    {
+
+
+        public List<string> Images
+        {
+            get { return (List<string>)GetValue(ImagesProperty); }
+            set { SetValue(ImagesProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Images.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImagesProperty =
+            DependencyProperty.Register("Images", typeof(List<string>), typeof(SliderView), new PropertyMetadata(null));
+
+
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CornerRadius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(SliderView), new PropertyMetadata(new CornerRadius(0)));
+
+
+        static SliderView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SliderView), new FrameworkPropertyMetadata(typeof(SliderView)));
+        }
+    }
+}
---
+++ IMDBApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IMDBApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var psi = new ProcessStartInfo
+{
+                FileName = "www.google.com",
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+
+    }
+}
